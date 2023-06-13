cc9b452c5d445b8b2dfc7d63ff9492a3141e41c9
saipranayteja <pranayteja.ch@algonox.com>
2023-06-13 21:30:28 +0530

    asdf

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id           = "${var.env}-elasticache-roboshop"
+  engine               = var.engine
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  engine_version       = var.engine_version
+  port                 = var.port
+  subnet_group_name    = aws_elasticache_subnet_group.elasticache-subnet.name
+  security_group_ids = [aws_security_group.elasticache-sg.id]
+}
+
+
+resource "aws_security_group" "elasticache-sg" {
+  name        = "${var.env}-roboshop-elasticache-SG"
+  description = "Security groups for elasticache"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = var.port
+    to_port          = var.port
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-roboshop-elasticache-SG"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "elasticache-subnet" {
+  name       = "${var.env}-elasticache-subnet"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "${var.env} elasticache subnet group"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "num_cache_nodes" {}
+variable "node_type"{}
+variable "port"{}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+variable "subnet_ids"{}
